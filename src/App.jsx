92c863e918fa9d7b64bea9ebffe14fbccf47cfa6
jae1 <t01046993182@gmail.com>
2026-05-07 17:51:32 +0900

    fix:router 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Terms from './components/Terms';
-import UserInfo from './components/UserInfo';
-import Main from './components/Main';
-import Chat from './components/Chat';
-import Chatroom from './components/Chatroom.jsx';
-import History from './components/History';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Terms from "./components/Terms";
+import UserInfo from "./components/UserInfo";
+import Main from "./components/Main";
+import Chat from "./components/Chat";
+import Chatroom from "./components/Chatroom.jsx";
+import History from "./components/History";
 
-
-
-
-import './App.css';
+import "./App.css";
 import MyPage from "./components/MyPage.jsx";
 
 function App() {
@@ -20,7 +17,7 @@ function App() {
         <BrowserRouter>
             <div className="App">
                 <Routes>
-                    <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/terms" element={<Terms />} />
                     <Route path="/userinfo" element={<UserInfo />} />
